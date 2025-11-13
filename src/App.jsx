6ebf95ca57d6caecde0ff09a3d31732602f30040
Ngbinh2005 <ngobinh3581@gmail.com>
2025-11-13 20:37:16 +0700

    Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useColorScheme } from '@mui/material/styles'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
 
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+  return (
+    <FormControl sx = {{ m: 1, minWidth: 120 }} size = "small">
+      <InputLabel id = "label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId = "label-select-dark-light-mode"
+        id = " select-dark-light-mode"
+        value = {mode}
+        label = "Mode"
+        onChange = {handleChange}
+      >
+        <MenuItem value = "light">
+          <div style = {{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize = "small" /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value = "dark">
+          <div style = {{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <DarkModeIcon fontSize = "small" /> Dark
+          </div>
+        </MenuItem>
+        <MenuItem value = "system">
+          <div style = {{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <SettingsBrightnessIcon fontSize = "small" /> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container disableGutters maxWidth = {false} sx = {{ height: '100vh', backgroundColor: 'primary.main' }}>
+      <Box sx = {{
+        backgroundColor: 'orange',
+        width: '100%',
+        height: '58px',
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect />
+      </Box>
+      <Box sx = {{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: '58px',
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx = {{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        height: 'calc(100vh - 58px * 2)'
+      }}
+      >
+        Box Content  
+      </Box>
+    </Container>
   )
 }
 
